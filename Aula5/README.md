d1ae40e59ba29ba059a878ad4316a10a6b12be76
a17214 <a17214@alunos.ipca.pt>
2022-11-25 21:34:44 +0000

    final add : aula 5

--- Aula5/README.md
+++ Aula5/README.md
@@ -3,16 +3,16 @@ Aula 5 - Prática
 
 ## Exercício 1
 Controlar a posição de rotação com um servo
-![Exercício 1](./img/exe3.jpeg)
+![Exercício 1](./img/exe1.jpeg)
 
 ## Exercício 2
 Controlar a velocidade de servos de rotação contínua
-![Exercício 2](./img/exe4.jpeg)
+![Exercício 2](./img/exe2.jpeg)
 
 ## Exercício 3
 Controlar servo motor através de sensor de proximidade ultrassónico
-![Exercício 3](./img/exe5.jpeg)
+![Exercício 3](./img/exe3.jpeg)
 
 ## Exercício 4
 Controlar servo com transístor Darlington TIP120
-![Exercício 4](./img/exe6.jpeg)
+![Exercício 4](./img/exe4.jpeg)
--- Aula5/exe4.ino
+++ Aula5/exe4.ino
@@ -1,92 +1,48 @@
-#include <Servo.h> 
-Servo servo;
+#include <Servo.h>
 
-int powerControl = 8;
-int servoPin = 10;
+Servo myservo;
+const int servo_pin = 7;
+const int trig_pin = 3;
+const int echo_pin = 2;
+const int inter_time = 200;
+int time = 0;
 
-void setup() { 
-  //Debug
+void setup()
+{
   Serial.begin(9600);
-  // pin 8 to base of Transistor
-  pinMode (powerControl, OUTPUT);
-  Serial.println("Transistor base set to pin "+ String (powerControl, DEC)); //pin 10 to Servo signal line
-  pinMode (servoPin, OUTPUT);
-  Serial.println("Servo signal set to pin " + String (servoPin, DEC));
+  myservo.attach(servo_pin, 500, 2400);
+  myservo.write(90);
+  pinMode (trig_pin, OUTPUT);
+  pinMode (echo_pin, INPUT);
+  delay(3000);
 }
 
-void loop() {
-  /****************************************
-  ATTACH servo signal to pin 10, turn 
-  ON servo power and MOVE (0 to 180)
-  ****************************************/
-  // turn on the pwm signal to the servo
-  servo.attach (servoPin);
-  Serial.println("Servo attached to " + String (servoPin, DEC));
-  
-  // turn on servo power
-  digitalWrite(powerControl, HIGH);
-  Serial.println("Servo Power is ON");
-  
-  //Sweep the servo 0 to 180
-  for (int servoPosition = 0; servoPosition < 181; servoPosition += 5) 
-  { 
-    servo.write(servoPosition);
-    Serial.println("Position:" + String (servoPosition, DEC));
-    delay(15);
-  }
-
-  //Wait for servo to execute last position
-  delay (500);
-
-  /****************************************
-  Turn OFF servo, Detach pin 10 
-  and WAIT for 3 seconds
-  ****************************************/
-  //turn off servo power
-  digitalWrite(powerControl, LOW); 
-  Serial.println("Servo power is OFF");
-
-  //turn off the pwm signal to the servo 
-  servo.detach ();
-  Serial.println("Servo detached");
-  
-  //wait for 1 second
-  delay (3000);
-  
-  /****************************************
-  ATTACH servo signal to pin 10, turn 
-  ON servo power and MOVE (0 to 180)
-  ****************************************/
-  // turn on the pwm signal to the servo
-  servo.attach (servoPin);
-  Serial.println("Servo attached to "+ String (servoPin, DEC));
-  digitalWrite (powerControl, HIGH);
-  
-   //turn on servo power
-  Serial.println("Servo Power is ON");
-  
-  //Sweep the servo 180 to 0
-  for (int servoPosition = 180; servoPosition >= 0; servoPosition -= 5) 
+void loop()
+{
+  float duration, distance;
+  digitalWrite(trig_pin, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trig_pin, LOW);
+  duration = pulseIN (echo_pin, HIGH);
+  distance = (duration/2)/29;
+  Serial.print(distance);
+  Serial.println("cm");
+  time = time + inter_time;
+  delay(inter_time);
+  if(distance <10)
   {
-    servo.write(servoPosition);
-    Serial.println("Position:" + String (servoPosition, DEC));
-    delay(15);
+    for(int i = 1500; i>=1100;i-=25)
+    {
+      myservo.writeMicroseconds(i);
+      Serial.println("2");
+      delay(100);
+    }
+    delay(1000);
+    for(int i = 1100; i <=1500; i+=25)
+    {
+      myservo.writeMicroseconds(i);
+      Serial.println("1");
+      delay(100);
+    }
   }
-
-  //Wait for servo to process last position 
-  delay (500);
-
-  /****************************************
-  Turn OFF servo, Detach pin 10 
-  and WAIT for 3 seconds
-  ****************************************/
-  // turn off servo power
-  digitalWrite (powerControl, LOW); Serial.println("Servo Power is OFF");
-  
-  //turn off the pwm signal to the servo servo.detach();
-  Serial.println("Servo detached");
-
-  //wait for 1 second
-  Serial.println("Waiting for 2 sec...");
-  delay(2000);
 }
